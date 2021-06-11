56674f613bd9cd774f2ae1b3e358fddc12437973
KamilHs <kamilsalimli1940@gmail.com>
2021-06-12 03:43:05 +0400

    refactor: restructure files

--- step3/console/board_drawer.h
+++
@@ -1,9 +0,0 @@
-#ifndef __BOARD_DRAWER_H__
-#define __BOARD_DRAWER_H__
-#include "life.h"
-
-void get_terminal_size(int *width, int *height);
-
-void draw_board(Game *game);
-
-#endif
--- step5/gui/gui.h
+++
@@ -1,7 +0,0 @@
-#ifndef __GUI_H__
-#define __GUI_H__
-#include "life.h"
-
-void draw_board(Game *game, SDL_Renderer *renderer);
-
-#endif
