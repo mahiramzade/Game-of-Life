0a10ca9abad2a04c89b75b9a2b5da81916b4ef81
mahiramzade <m.malikzade@gmail.com>
2021-06-12 03:42:32 +0400

    refactor: added comments to step3

--- step3/console/board_drawer.h
+++ step3/console/board_drawer.h
@@ -2,8 +2,20 @@
 #define __BOARD_DRAWER_H__
 #include "life.h"
 
+/**
+ * Gets terminal width and height and assign them
+ * to the appropriate memory addresses
+ * @param width address of an integer
+ * @param height address of an integer 
+ * @return void
+*/
 void get_terminal_size(int *width, int *height);
 
+/**
+ * Prints the game's board on the terminal
+ * @param game - Pointer to the game object
+ * @return void
+*/
 void draw_board(Game *game);
 
 #endif
--- step3/game/life.c
+++ step3/game/life.c
@@ -2,11 +2,21 @@
 #include <time.h>
 #include "life.h"
 
+/** 
+ * @param min - minimum value for range
+ * @param max - maximum value for range
+ * @return random integer value between min and max 
+*/
 int randInRange(int min, int max)
 {
     return min + (int)(rand() / (double)(RAND_MAX + 1.0) * (max - min + 1));
 }
 
+/** 
+ * @param rows - number of rows
+ * @param cols - number of columns
+ * @return dynamically allocated two dimensional integer array 
+*/
 int **allocate2dArr(int rows, int cols)
 {
     int **arr = (int **)calloc(sizeof(int *), rows);
@@ -15,6 +25,12 @@ int **allocate2dArr(int rows, int cols)
     return arr;
 }
 
+/** 
+ * @param arr - 2D integer array
+ * @param rows - number of rows
+ * @param cols - number of columns
+ * @return dynamically allocated copy of arr 
+*/
 int **copy2dArr(int **arr, int rows, int cols)
 {
     int **copy = allocate2dArr(rows, cols);
@@ -24,6 +40,12 @@ int **copy2dArr(int **arr, int rows, int cols)
     return copy;
 }
 
+/**
+ * @param game - Pointer to the game object
+ * @param row - the row of the cell
+ * @param col - the column of the cell
+ * @return number of the alive neighbours of the cell at row and col
+ */
 int _getAliveNeighbours(Game *game, int row, int col, int is_clipped)
 {
     if (!game)
@@ -49,6 +71,11 @@ int _getAliveNeighbours(Game *game, int row, int col, int is_clipped)
     return aliveCount;
 }
 
+/**
+ * @param rows - number of rows of the board
+ * @param cols - number of columns of the board
+ * @return pointer to the dynamically allocated game object with the board of size rows and cols
+*/
 Game *G_new(int rows, int cols)
 {
     Game *game = (Game *)malloc(sizeof(Game));
@@ -59,6 +86,11 @@ Game *G_new(int rows, int cols)
     return game;
 }
 
+/**
+ * Randomly initializes the cells of the game's board
+ * @param game - pointer to the game object
+ * @return void
+*/
 void G_generate(Game *game)
 {
     if (!game)
@@ -72,6 +104,11 @@ void G_generate(Game *game)
             board[i][j] = randInRange(0, 1) < LIVE_PROB;
 }
 
+/**
+ * Creates new generation according to the game's rules
+ * @param game - Pointer to the game object
+ * @return void
+*/
 void G_live(Game *game, int is_clipped)
 {
     if (!game)
