ccc70dbd5e6df1cc74e48c55f79d0e779fd94c54
mahiramzade <m.malikzade@gmail.com>
2021-06-12 03:54:20 +0400

    refactor:added comments

--- game/life.h
+++ game/life.h
@@ -3,17 +3,38 @@
 
 #define LIVE_PROB 0.6
 
+/** structure of the game */
 typedef struct game
 {
+    /** current generation of the game*/
     int generation;
-    int rows, cols;
+    /** number of rows of the game board */
+    int rows;
+    /** number of cols of the game board */
+    int cols;
+    /** two dimensional array representing game board */
     int **board;
 } Game;
 
+/**
+ * @param rows - number of rows of the board
+ * @param cols - number of columns of the board
+ * @return pointer to the dynamically allocated game object with the board of size rows and cols
+*/
 Game *G_new(int rows, int cols);
 
+/**
+ * Randomly initializes the cells of the game's board
+ * @param game - pointer to the game object
+ * @return void
+*/
 void G_generate(Game *game);
 
+/**
+ * Creates new generation according to the game's rules
+ * @param game - Pointer to the game object
+ * @return void
+*/
 void G_live(Game *game, int is_clipped);
 
 #endif
